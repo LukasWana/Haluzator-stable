4aa6ff5180eb31c3ab5a5d2584c06e855f2d5928
Lukas <qwanap@gmail.com>
2025-11-20 21:38:29 +0100

    pridano 33 novych shaderu

--- shaders/index.ts
+++ shaders/index.ts
@@ -2,9 +2,14 @@
 
 import AlienTube from './AlienTube';
 import AlienWaterworld from './AlienWaterworld';
+import AtomSys from './AtomSys';
 import AtansBegone from './AtansBegone';
+import AudioReactiveHalo from './AudioReactiveHalo';
 import AudioRings from './AudioRings';
 import AuroraBorealis from './AuroraBorealis';
+import AuroraRing from './AuroraRing';
+import BioLinks from './BioLinks';
+import BipolarPatterns from './BipolarPatterns';
 import BlackHole from './BlackHole';
 import BouncingBall from './BouncingBall';
 import BoxSphereMarch from './BoxSphereMarch';
@@ -12,6 +17,7 @@ import Bubblegum from './Bubblegum';
 import Building from './Building';
 import ChromaKelp from './ChromaKelp';
 import CityFlyover3D from './CityFlyover3D';
+import CloudTen from './CloudTen';
 import CloudySky from './CloudySky';
 import CogWheels from './CogWheels';
 import ColorMandala from './ColorMandala';
@@ -19,11 +25,17 @@ import ComplicatedThings from './ComplicatedThings';
 import CosmicCanvas from './CustomShaders';
 import CosmicEgg from './CosmicEgg';
 import CosmicJourney from './CosmicJourney';
+import CosmicRays from './CosmicRays';
+import CosmicSwirl from './CosmicSwirl';
+import CubeTunnel from './CubeTunnel';
+import CyberTube from './CyberTube';
+import DataPatterns from './DataPatterns';
 import DaveHoskinsExpert from './DaveHoskinsExpert';
 import DitheredFluid from './DitheredFluid';
 import DyingStars from './DyingStars';
 import DysonSwarm from './DysonSwarm';
 import ElectricFilaments from './ElectricFilaments';
+import EnchantedForest from './EnchantedForest';
 import ExplodingStars from './ExplodingStars';
 import FibonacciSpheres from './FibonacciSpheres';
 import FlyingStars from './FlyingStars';
@@ -47,12 +59,16 @@ import HelixDNA from './HelixDNA';
 import HelixDNAGlow from './HelixDNAGlow';
 import HexMarching from './HexMarching';
 import HypnoticGears from './HypnoticGears';
+import Impact from './Impact';
+import InfiniteDream from './InfiniteDream';
 import InfiniteFractal3D from './InfiniteFractal3D';
+import Interstellar from './Interstellar';
 import InnerReflections from './InnerReflections';
 import InsideMandelbulb from './InsideMandelbulb';
 import InkBleed from './InkBleed';
 import IsoCity from './IsoCity';
 import JourneyTribute from './JourneyTribute';
+import Kaleidoscope from './Kaleidoscope';
 import LightPrism from './LightPrism';
 import Liquid from './Liquid';
 import LogarithmicSpirals from './LogarithmicSpirals';
@@ -62,10 +78,13 @@ import MagicMush from './MagicMush';
 import MatrixRain from './MatrixRain';
 import MandelbrotAA from './MandelbrotAA';
 import MandelbrotZoom from './MandelbrotZoom';
+import Mandelbulb from './Mandelbulb';
 import MandelbulbAudio from './MandelbulbAudio';
 import MandelboxVariant from './MandelboxVariant';
+import Matrix from './Matrix';
 import MechStruct from './MechStruct';
 import MindFlowers from './MindFlowers';
+import Mist from './Mist';
 import Mushroom from './Mushroom';
 import MushroomTunnel from './MushroomTunnel';
 import MyceliumGrowth from './MyceliumGrowth';
@@ -82,7 +101,9 @@ import OrganicMandala from './OrganicMandala';
 import OrigamiField from './OrigamiField';
 import OversaturatedWeb from './OversaturatedWeb';
 import PinkWorms from './PinkWorms';
+import Plasma from './Plasma';
 import PlanetSystem from './PlanetSystem';
+import PsyPalette from './PsyPalette';
 import PulsatingStar from './PulsatingStar';
 import QuantumFoam from './QuantumFoam';
 import QuantumVortex from './QuantumVortex';
@@ -90,38 +111,60 @@ import QuaternionMandelbulb from './QuaternionMandelbulb';
 import RadiatingCircles from './RadiatingCircles';
 import RainbowShroom from './RainbowShroom';
 import RefractionMadness from './RefractionMadness';
+import RhombusLines from './RhombusLines';
 import SaturdayTorus from './SaturdayTorus';
 import SeaAndMoon from './SeaAndMoon';
 import ShapeMorphGrid from './ShapeMorphGrid';
+import SimplexFlame from './SimplexFlame';
+import SkyReflection from './SkyReflection';
 import SolarFlare from './SolarFlare';
 import SoundWaves from './SoundWaves';
 import SpacePlace from './SpacePlace';
 import SphereRecursion from './SphereRecursion';
+import SpiralVortex from './SpiralVortex';
 import StarNest from './StarNest';
+import StarNursery from './StarNursery';
 import StarTunnel from './StarTunnel';
 import Starnova from './Starnova';
 import StarfieldZoom from './StarfieldZoom';
 import StellarWarp from './StellarWarp';
+import SunEarth from './SunEarth';
 import SynapticOrb from './SynapticOrb';
 import Tesseract4D from './Tesseract4D';
-import TuesdayInfiniteZoom from './TuesdayInfiniteZoom';
+import TheGrid from './TheGrid';
+import TheTrip from './TheTrip';
+import ToonFractal from './ToonFractal';
+import ToxicPlanet from './ToxicPlanet';
 import Transwarp from './Transwarp';
+import TriangleKaleidoscope from './TriangleKaleidoscope';
+import TruchetTubes from './TruchetTubes';
+import TuesdayInfiniteZoom from './TuesdayInfiniteZoom';
+import Tunnelwisp from './Tunnelwisp';
 import UltimateFlame from './UltimateFlame';
+import Voronoi from './Voronoi';
 import VoronoiGlass from './VoronoiGlass';
+import Vortex from './Vortex';
 import WarpDrive from './WarpDrive';
 import WaterSphere from './WaterSphere';
 import WeavingWorms from './WeavingWorms';
 import WindowsTerminal from './WindowsTerminal';
-import WovenTapestry from './WovenTapestry';
+import Wires from './Wires';
+import Worms from './Worms';
 import WormStructures from './WormStructures';
+import WovenTapestry from './WovenTapestry';
 
 
 const sources = {
   'AlienTube': AlienTube,
   'AlienWaterworld': AlienWaterworld,
+  'AtomSys': AtomSys,
   'AtansBegone': AtansBegone,
+  'AudioReactiveHalo': AudioReactiveHalo,
   'AudioRings': AudioRings,
   'AuroraBorealis': AuroraBorealis,
+  'AuroraRing': AuroraRing,
+  'BioLinks': BioLinks,
+  'BipolarPatterns': BipolarPatterns,
   'BlackHole': BlackHole,
   'BouncingBall': BouncingBall,
   'BoxSphereMarch': BoxSphereMarch,
@@ -129,6 +172,7 @@ const sources = {
   'Building': Building,
   'ChromaKelp': ChromaKelp,
   'CityFlyover3D': CityFlyover3D,
+  'CloudTen': CloudTen,
   'CloudySky': CloudySky,
   'CogWheels': CogWheels,
   'ColorMandala': ColorMandala,
@@ -136,11 +180,17 @@ const sources = {
   'CosmicCanvas': CosmicCanvas,
   'CosmicEgg': CosmicEgg,
   'CosmicJourney': CosmicJourney,
+  'CosmicRays': CosmicRays,
+  'CosmicSwirl': CosmicSwirl,
+  'CubeTunnel': CubeTunnel,
+  'CyberTube': CyberTube,
+  'DataPatterns': DataPatterns,
   'DaveHoskinsExpert': DaveHoskinsExpert,
   'DitheredFluid': DitheredFluid,
   'DyingStars': DyingStars,
   'DysonSwarm': DysonSwarm,
   'ElectricFilaments': ElectricFilaments,
+  'EnchantedForest': EnchantedForest,
   'ExplodingStars': ExplodingStars,
   'FibonacciSpheres': FibonacciSpheres,
   'FluidStars': FluidStars,
@@ -164,12 +214,16 @@ const sources = {
   'HelixDNAGlow': HelixDNAGlow,
   'HexMarching': HexMarching,
   'HypnoticGears': HypnoticGears,
+  'Impact': Impact,
+  'InfiniteDream': InfiniteDream,
   'InfiniteFractal3D': InfiniteFractal3D,
+  'Interstellar': Interstellar,
   'InkBleed': InkBleed,
   'InnerReflections': InnerReflections,
   'InsideMandelbulb': InsideMandelbulb,
   'IsoCity': IsoCity,
   'JourneyTribute': JourneyTribute,
+  'Kaleidoscope': Kaleidoscope,
   'LightPrism': LightPrism,
   'Liquid': Liquid,
   'LogSprlEvo': LogSprlEvo,
@@ -179,10 +233,13 @@ const sources = {
   'MandelboxVariant': MandelboxVariant,
   'MandelbrotAA': MandelbrotAA,
   'MandelbrotZoom': MandelbrotZoom,
+  'Mandelbulb': Mandelbulb,
   'MandelbulbAudio': MandelbulbAudio,
+  'Matrix': Matrix,
   'MatrixRain': MatrixRain,
   'MechStruct': MechStruct,
   'MindFlowers': MindFlowers,
+  'Mist': Mist,
   'Mushroom': Mushroom,
   'MushroomTunnel': MushroomTunnel,
   'MyceliumGrowth': MyceliumGrowth,
@@ -199,7 +256,9 @@ const sources = {
   'OrigamiField': OrigamiField,
   'OversaturatedWeb': OversaturatedWeb,
   'PinkWorms': PinkWorms,
+  'Plasma': Plasma,
   'PlanetSystem': PlanetSystem,
+  'PsyPalette': PsyPalette,
   'PulsatingStar': PulsatingStar,
   'QuantumFoam': QuantumFoam,
   'QuantumVortex': QuantumVortex,
@@ -207,28 +266,45 @@ const sources = {
   'RadiatingCircles': RadiatingCircles,
   'RainbowShroom': RainbowShroom,
   'RefractionMadness': RefractionMadness,
+  'RhombusLines': RhombusLines,
   'SaturdayTorus': SaturdayTorus,
   'SeaAndMoon': SeaAndMoon,
   'ShapeMorphGrid': ShapeMorphGrid,
+  'SimplexFlame': SimplexFlame,
+  'SkyReflection': SkyReflection,
   'SolarFlare': SolarFlare,
   'SoundWaves': SoundWaves,
   'SpacePlace': SpacePlace,
   'SphereRecursion': SphereRecursion,
+  'SpiralVortex': SpiralVortex,
   'StarNest': StarNest,
+  'StarNursery': StarNursery,
   'StarTunnel': StarTunnel,
   'StarfieldZoom': StarfieldZoom,
   'Starnova': Starnova,
   'StellarWarp': StellarWarp,
+  'SunEarth': SunEarth,
   'SynapticOrb': SynapticOrb,
   'Tesseract4D': Tesseract4D,
+  'TheGrid': TheGrid,
+  'TheTrip': TheTrip,
+  'ToonFractal': ToonFractal,
+  'ToxicPlanet': ToxicPlanet,
   'Transwarp': Transwarp,
+  'TriangleKaleidoscope': TriangleKaleidoscope,
+  'TruchetTubes': TruchetTubes,
   'TuesdayInfiniteZoom': TuesdayInfiniteZoom,
+  'Tunnelwisp': Tunnelwisp,
   'UltimateFlame': UltimateFlame,
+  'Voronoi': Voronoi,
   'VoronoiGlass': VoronoiGlass,
+  'Vortex': Vortex,
   'WarpDrive': WarpDrive,
   'WaterSphere': WaterSphere,
   'WeavingWorms': WeavingWorms,
   'WindowsTerminal': WindowsTerminal,
+  'Wires': Wires,
+  'Worms': Worms,
   'WormStructures': WormStructures,
   'WovenTapestry': WovenTapestry,
 };
